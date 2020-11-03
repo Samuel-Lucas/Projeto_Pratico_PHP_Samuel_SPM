e7b4d16f3a1a951e4c8631d38f29eec3e09409ae
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2020-11-03 14:32:58 -0300

    banco de dados utilizado para o projeto

---
+++ arquivo_sql/db_projeto_php_samuel_spm.sql
@@ -0,0 +1,87 @@
+CREATE DATABASE  IF NOT EXISTS `projeto_php` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `projeto_php`;
+-- MySQL dump 10.16  Distrib 10.1.39-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: projeto_php
+-- ------------------------------------------------------
+-- Server version	10.1.39-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedidos` (
+  `idpedido` int(11) NOT NULL AUTO_INCREMENT,
+  `nomecliente` varchar(70) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `nomeproduto` varchar(300) NOT NULL,
+  `valor_unit` decimal(10,2) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valor_total` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idpedido`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedidos`
+--
+
+LOCK TABLES `pedidos` WRITE;
+/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
+INSERT INTO `pedidos` VALUES (1,'Fulano','Rua qualquer','1163529987','3499.00',3499.00,2,6998.00),(4,'Fulana','Rua das Margaridas, 38','1144552266','4499.00',4499.00,1,4499.00),(5,'Samuel','Rua itapevi, 58','1153625599','4499.00',4499.00,10,44990.00);
+/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produtos` (
+  `idproduto` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(70) NOT NULL,
+  `descricao` varchar(500) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `preco_venda` decimal(10,2) NOT NULL,
+  `imagem` varchar(50) NOT NULL,
+  PRIMARY KEY (`idproduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (1,'computadores','Pc Gamer G-Fire Htg-686 A6 7480 8Gb (Radeon R5 2Gb) SSD 240Gb monitor 18',4199.00,3499.00,'images/pc.jpg'),(2,'notebooks','Notebook Gamer Lenovo Intel Core i7-9750H 8GB 1TB Placa GTX 1050 monitor 15 Windows 10 L340',4399.00,3899.00,'images/note.jpg'),(3,'videogames','Console Playstation 5 SSD 1 TB 1 controle',4999.00,4499.00,'images/ps5.jpg'),(4,'acessorios','Teclado Mecanico Gamer Redragon Kala RGB, Switch Red, ABNT-2, K557RGB',159.00,119.00,'images/teclado_mecanico.jpg'),(5,'videogames','Console Xbox Series X SSD 1 TB 1 controle',4999.00,4499.00,'images/xsx.jpg'),(6,'acessorios','Mouse Gamer Redragon Ranger Black RGB 12400dpi M910-RGB',159.00,99.00,'images/mouse.jpg');
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-03 14:29:53
